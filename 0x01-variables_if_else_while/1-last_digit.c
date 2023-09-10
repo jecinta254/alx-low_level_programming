c8d9160b3112b26d92effb320bef8145baf6d0c8
Jecinta <njokijecinta254@gmail.com>
2023-09-10 14:44:28 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+/**
  * main - Entry point
  *This program will assign a random number to the variable n each time it is executed
- * return 0 Always */
+ *Return: always 0
+ */
 int main(void)
 {
 	int n;

4239dc4796ae88179d426bbc358e77719847ea60
Jecinta <njokijecinta254@gmail.com>
2023-09-12 09:17:22 -0700

    102-print_comb5.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * main - prints the alphabet in lowercase
+ * print_alphabet - prints the alphabet in lowercase
  * Return: return print_alphabet
  */
 void print_alphabet(void)
-{
-	char n;
 
-	for (n = 'a' ; n <= 'z' ; n++)
-	_putchar(n);
-	_putchar('\n');
+{
+char c;
+
+for (c = 'a' ; c <= 'z' ; c++)
+_putchar(c);
+_putchar('\n');
 }

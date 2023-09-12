a385e797c8b3432be7edde6327d61136ff11510a
Jecinta <njokijecinta254@gmail.com>
2023-09-12 09:49:05 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints alphabets lowercase 10 times
+*Return: return 0
+*/
+void print_alphabet_x10(void)
+{
+	int m;
+	char n;
+
+	for (m = 1 ; m < 10 ; m++)
+	{
+		for (n = 'a' ; n <= 'z' ; n++)
+			_putchar(n);
+		_putchar('\n');
+	}
+}

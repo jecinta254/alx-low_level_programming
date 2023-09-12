ff50eed1bd5289544560e7d0d084e22ba26bcf6c
Jecinta <njokijecinta254@gmail.com>
2023-09-12 16:25:41 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: function parameter
+ * Return: m
+ */
+
+int print_last_digit(int)
+{
+int m;
+m = n % 10;
+if (n < 0)
+	m = -m;
+_putchar(m + '0');
+return (m);
+}

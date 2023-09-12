91d237cefa1ee1a2c6486f65c763e536e39c6168
Jecinta <njokijecinta254@gmail.com>
2023-09-12 05:40:37 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints out the alphabet in lowercase
+ * Return: return 0
+ */
+void print_alphabet(void)
+{
+	char[26]="abcdefghijklmnopqrstuvwxyz"
+	{
+		_putchar("abcdefghijklmnopqrstuvwxyz")
+	}
+	_putchar('\n');
+	return (0);
+}

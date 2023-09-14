58142b6b208385221495d7ba4e567f6bad9f0611
Jecinta <njokijecinta254@gmail.com>
2023-09-14 03:45:58 -0700

    i believe in numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_number - prints the numbers, from 0 to 9.
+ * print_most_numbers - prints the numbers, from 0 to 9.
  * return: Return void
  */
 void print_most_numbers(void)

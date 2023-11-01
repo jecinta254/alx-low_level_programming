bff323f856d9ac59aacfb28663c69f1b6e5eda90
jecinta254 <njokijecinta254@gmail.com>
2023-11-01 09:01:27 -0400

    0x15. C - File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,25 +1,43 @@
 #include "main.h"
-
 /**
- * read_textfile - Reading text file and prints to the POSIX standard output
- * @filename: name of the fie to read
- * @letters: letters to read and print
- * Return: number of letters
+ * read_textfile -This program will read and print text
+ * From a file to stdout.
+ * @filename:Name of the file created.
+ * @letters:Number of letters to be read and printed in the file.
+ * Return:Actual number of read and printed letters.
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int our_file;
-ssize_t byts;
-char buf[READ_BUF_SIZE * 8];
+	int our_file;
+	char *s;
+	ssize_t read_length, write_length;
 
-if (!filename || !letters)
-	return (0);
-our_file = open(filename, O_RDONLY);
-if (our_file == -1)
-	return (0);
-byts = read(our_file, &buf[0], letters);
-byts = write(STDOUT_FILENO, &buf[0], byts);
-close(our_file);
-return (byts);
+	if (!filename)
+		return (0);
+	our_file = open(filename, O_RDONLY);
+	if (our_file < 0)
+		return (0);
+
+	s = malloc(sizeof(char) * letters);
+	if (!s)
+		return (0);
+	read_length = read(our_file, s, letters);
+	if (read_length < 0)
+	{
+		free(s);
+		return (0);
+	}
+
+	s[read_length] = '\0';
+	close(our_file);
+
+	write_length = write(STDOUT_FILENO, s, read_length);
+	if (write_length < 0)
+	{
+		free(s);
+		return (0);
+	}
+	free(s);
+	return (write_length);
 }

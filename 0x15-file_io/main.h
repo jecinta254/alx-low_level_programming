8127766c56c39e4d1248dcbd80d2c46b90063f4f
jecinta254 <njokijecinta254@gmail.com>
2023-11-02 08:20:55 -0400

    0x15. C - File I/O

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,11 +15,6 @@
 
 #define handle_error(msg) \
 
-do {
-perror(EXIT_FAILURE);
-}
-while (0)
-
 ssize_t read_textfile(const char *filename, size_t letters);
 
 int create_file(const char *filename, char *text_content);

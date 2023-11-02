cc66fd09bdf2d9ed5c5894a5b32a5189b6bd0a61
jecinta254 <njokijecinta254@gmail.com>
2023-11-01 23:40:45 -0400

    file I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,25 +1,47 @@
 #include "main.h"
 
-void check_IO_stst(int stat, int fd, char *filename, char mode);
-/*
- * main - copy content of one file to another
- * @argc: The count
- * @argv: Argument passed
- * Return: 1 success -1 failure
+/**
+ * main - Copies content of a file to another file.
+ * @ac: Argument count.
+ * @av: Argument vector (array of strings {file_names}) .
+ *
+ * Return: always 0 (Success)
  */
-int main(int arg, char *argv[1])
+int main(int ac, char **av)
 {
-	int src, dest, n_read = 1024, wrote, close_src, close_dest;
-	unsigned int mode = s_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+	int ourfile_from, ourfile_to;
 	char buffer[1024];
+	ssize_t i, j;
 
-	if (arg != 3)
+	if (ac != 3)
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+
+	ourfile_from = open(av[1], O_RDONLY);
+	ourfile_to = open(av[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);
+	if (ourfile_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	if (ourfile_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+
+	do {
+		i = read(ourfile_from, buffer, 1024);
+		if (i == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		j = write(ourfile_to, buffer, i);
+		if (j == -1 || j != i)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+	} while (i);
+
+
+	i = close(ourfile_from), j = close(ourfile_to);
+	if (i || j)
 	{
-		dprintf(STDERR_FILENO, "%s", "usage: cp file_from file-to\n");
-		exit(97);
+		if (i)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ourfile_from);
+		if (j)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ourfile_to);
+		exit(100);
 	}
-	src = open(argv[1], O_RDONLT);
-	check_IO_stat(src, -1, argv[21, 'o');
-	dest = open(argv[2], O_WRONGLY | O_CREATE | O_TRNIC, mode);
-	check_IO_stat(dest, -1, argv[2], 'w');
+
+	return (0);
 }

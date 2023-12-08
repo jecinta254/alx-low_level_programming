a3db4c837281f9aa213ddf9024c6c39503574971
jecinta254 <140118106+jecinta254@users.noreply.github.com>
2023-12-07 22:00:19 -0500

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
  * dlistint_len - returns the number of elements in
  * a double linked list
  *
- * @h: head of the list
+ * @h: head of the list input
  * Return: the number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)

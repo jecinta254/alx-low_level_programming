a20ddd6f09cfa5e64c5c8c6514f116a3eaf3be2b
jecinta254 <140118106+jecinta254@users.noreply.github.com>
2023-12-08 06:22:17 +0300

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -4,7 +4,7 @@
  * delete_dnodeint_at_index - deletes the node at index of a
  * dlistint_t linked list
  *
- * @head: head of the list
+ * @head: head of the list input
  * @index: index of the new node
  * Return: 1 if it succeeded, -1 if it failed
  */
